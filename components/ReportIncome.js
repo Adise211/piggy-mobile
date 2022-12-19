e3a94fb369416d3d692b3844b1502d5f4f3e5054
Adise211 <101215614+Adise211@users.noreply.github.com>
2022-12-19 23:43:42 +0200

    ReportIncome component

--- components/ReportIncome.js
+++ components/ReportIncome.js
@@ -1,17 +1,68 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput } from "react-native";
 import { COLORS } from "./constants";
 import SaveButton from "../UI/SaveButton";
 
 const ReportIncome = () => {
     return (
-        <View>
-            <Text>Report income</Text>
+        <View style={styles.inContainer}>
+            <Text style={styles.title}>Add Your Income</Text>
+            <View style={styles.inputs}>
+                <Text>Title:</Text>
+                <TextInput style={styles.input} placeholder="Salary"/>
+                <Text>Amount:</Text>
+                <TextInput style={styles.input} />
+                <Text>Date:</Text>
+                <TextInput style={styles.input} placeholder="DD/MM/YYYY"/>
+            </View>
+            <View style={styles.buttonsContainer}>
+                <SaveButton style={styles.save}>Save</SaveButton>
+                {/* <SaveButton style={styles.back} onPress={() => goBackHandler()}>Back</SaveButton> */}
+            </View>
+
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-   
+    inContainer: {
+        marginTop: 50
+    },
+    title: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        textAlign:'center',
+        marginBottom: 40
+    },
+    inputs: {
+        flexDirection: 'column',
+        alignItems: 'center',
+        marginBottom: 30
+    },
+    input: {
+        height: 50,
+        backgroundColor: COLORS.white,
+        borderWidth: 1,
+        width: 250,
+        marginBottom: 20,
+        borderRadius: 8,
+        padding: 10
+    },
+    buttonsContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center'
+    },
+    save: {
+        padding: 10,
+        backgroundColor: COLORS.primary_pink,
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold',
+        width: 100,
+        textAlign: 'center',
+        borderRadius: 8,
+        elevation: 5
+    },
 })
 
 export default ReportIncome;
--- screens/IncomeOutcome.js
+++ screens/IncomeOutcome.js
@@ -20,10 +20,6 @@ const IncomeOutcome = ({ route }) => {
                     <ReportOutcome />
                 )}
             </View>
-            <View style={styles.buttonsContainer}>
-                <SaveButton style={styles.save}>Save</SaveButton>
-                {/* <SaveButton style={styles.back} onPress={() => goBackHandler()}>Back</SaveButton> */}
-            </View>
         </View>
     )
 };
@@ -31,24 +27,6 @@ const IncomeOutcome = ({ route }) => {
 const styles = StyleSheet.create({
     container: {
         marginTop: 50
-    },
-    buttonsContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        width: 200,
-        position: 'relative',
-        left: 80
-    },
-    save: {
-        padding: 10,
-        backgroundColor: COLORS.primary_pink,
-        color: 'white',
-        fontSize: 16,
-        fontWeight: 'bold',
-        width: 100,
-        textAlign: 'center',
-        borderRadius: 8,
-        elevation: 5
     },
     // back: {
     //     padding: 10,
